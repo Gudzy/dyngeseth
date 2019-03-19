d10fcd17783d97a65aea77059dc6bfb784ba2263
gudzy <gustdyn@gmail.com>
2019-03-19 20:20:26 +0100

    first commit

---
+++ client/src/views/Components/Components.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react components for routing our app without refresh
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// @material-ui/icons
+// core components
+import Header from "components/Header/Header.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+// sections for this page
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import SectionCarousel from "./Sections/SectionCarousel.jsx";
+import SectionCompletedExamples from "./Sections/SectionCompletedExamples.jsx";
+
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+
+class Components extends React.Component {
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { classes, ...rest } = this.props;
+    return (
+      <div>
+        <Header
+          brand="Gustav Dyngeseth"
+          rightLinks={<HeaderLinks />}
+          fixed
+          color="transparent"
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+          {...rest}
+        />
+        <Parallax image={require("assets/img/bg4.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem>
+                <div className={classes.brand}>
+                  <h1 className={classes.title}>Gustav Dyngeseth.</h1>
+                  <h3 className={classes.subtitle}>
+                    Consultant & Software Developer
+                  </h3>
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <SectionCompletedExamples />
+          <SectionCarousel />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withStyles(componentsStyle)(Components);
